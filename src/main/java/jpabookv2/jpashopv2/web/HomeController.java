670825429bd10624d22836933a771b08838c4a81
jngbnss <jngbnmn@naver.com>
2025-09-04 17:05:06 +0900

    5:04 멤버 컨트롤러까지 진행함

---
+++ src/main/java/jpabookv2/jpashopv2/web/HomeController.java
@@ -0,0 +1,15 @@
+package jpabookv2.jpashopv2.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+public class HomeController {
+    @RequestMapping("/")
+    public String hemo(){
+        log.info("home controller");
+        return "home";
+    }
+}
---
+++ src/main/java/jpabookv2/jpashopv2/web/MemberController.java
@@ -0,0 +1,17 @@
+package jpabookv2.jpashopv2.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberService memberService;
+
+    @GetMapping(value = "/members/new")
+    public String creatForm(Model model){
+        model.addAttribute("memberForm", new MemberForm());
+        return "memberes/createMemberForm";
+    }
+}
