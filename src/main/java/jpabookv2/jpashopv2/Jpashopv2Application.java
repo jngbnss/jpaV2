e2514759f976875deb8a15215ec31a6774d0a5dc
jngbnss <jngbnmn@naver.com>
2025-09-04 16:25:13 +0900

    Initial commit from IntelliJ

---
+++ src/main/java/jpabookv2/jpashopv2/Jpashopv2Application.java
@@ -0,0 +1,13 @@
+package jpabookv2.jpashopv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jpashopv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jpashopv2Application.class, args);
+	}
+
+}
