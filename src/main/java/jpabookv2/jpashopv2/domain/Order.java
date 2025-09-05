a42c484e883b5f0eea5418b4f3409c7ba546b7fb
jngbn <[user email]>
2025-09-05 13:21:58 +0900

    2.도메인 분석 설계 완료

--- src/main/java/jpabookv2/jpashopv2/domain/Order.java
+++ src/main/java/jpabookv2/jpashopv2/domain/Order.java
@@ -17,16 +17,17 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id") //맵핑을 뭐로 할거냐 fk이름
     private Member member;
     // 값은 여기서 바꿔야하지
     //연관관계의 주인
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order",cascade = CascadeType.ALL) // persist를 전파해줌
     private List<OrderItem> orderItems = new ArrayList<>();
+    // 라이프 사이클이 부모에게 종속적이면
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
     // 오더 테이블에 딜리버리를 fk로 가지고 있으니까 연관관계의 주인으로
@@ -38,4 +39,18 @@ public class Order {
     private OrderStatus status;
 
     // 연관관계 메서드 왜 해야하는지 작성해봐
+    //양방향 연관관계를 “일관성 있게” 맞춰주기 위해서예요.
+    //== 연관관계 편의 메서드==//
+    public void setMember(Member member){
+        this.member = member;
+        member.getOrders().add(this);
+    }
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+    public void setDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
